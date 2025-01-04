09ff5dfdedba680d6022e29c97c4cb04008621f6
SNOW <bladetrinity466@gmail.com>
2025-01-03 21:10:16 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export PATH=$(pwd)/pronton-toolchains-a33/clang/host/linux-x86/clang-r416183b/bi
 export PATH=$(pwd)/pronton-toolchains-a33/build/build-tools/path/linux-x86:$(pwd)/pronton-toolchains-a33/prebuilts/gas/linux-x86:$PATH
 
 make PLATFORM_VERSION=14 ANDROID_MAJOR_VERSION=u LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/pronton-toolchains-a33/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- s5e8825-a33xub_defconfig
-make PLATFORM_VERSION=14 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/pronton-toolchains-a33/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- -j32
+make PLATFORM_VERSION=14 ANDROID_MAJOR_VERSION=u LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/pronton-toolchains-a33/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- -j32
